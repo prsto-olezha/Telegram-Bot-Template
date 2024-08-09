9ee3ca29547eed26d3ac15f7ffa37b5622a3c5e8
MicRofaRatOV <75671540+MicRofaRatOV@users.noreply.github.com>
2024-08-09 20:27:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,26 +9,24 @@ from src.TWR import ThreadWithReturn
 from scripts.handlers import router
 from scripts.admin_handlers import router as admin_router
 import src.FSM as FSM
-#===========================================================
-# Объект бота
+
+
 bot = Bot(token=token.TOKEN)
-# Диспетчер
 dp = Dispatcher(storage=FSM.storage)
 
 
-def default_timer():
-    # Настраиваем исполнение задачи каждый час
+def log_timer():
     schedule.every().day.at("00:00", tz="Europe/Moscow").do(switch_logfile, datetime.datetime.now().date())
-    # Бесконечный цикл, чтобы гарантировать выполнение
     while True:
         schedule.run_pending()
+
 
 def switch_logfile(date: datetime.date):
     logging.basicConfig(filename=f"src/logs/{date}.log",
                         level=logging.INFO,
                         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-# Запуск процесса поллинга новых апдейтов
+
 async def main():
     dp.include_router(router)
     dp.include_router(admin_router)
@@ -43,7 +41,7 @@ if __name__ == "__main__":
         print()
 
         threads = [
-            ThreadWithReturn(target=default_timer)
+            ThreadWithReturn(target=log_timer)
         ]
 
         for thread in threads:
